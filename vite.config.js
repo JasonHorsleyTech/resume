7ff223ddb7101662d03913b358ed2133b84bf45a
Jason Horsley <32709073+JasonHorsleyTech@users.noreply.github.com>
2021-11-08 13:57:45 -0600

    Build to public/dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default ({ command }) => ({
     publicDir: "disable",
-    base: command === "build" ? "/dist/" : "",
+    base: command === "build" ? "/public/dist/" : "",
     plugins: [
         vue(),
     ],
