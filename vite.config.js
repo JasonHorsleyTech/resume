57e05c2ba8ebcf4bf9fd6dd5fb71bb3f766d6da3
Jason Horsley <32709073+JasonHorsleyTech@users.noreply.github.com>
2021-11-08 14:25:38 -0600

    Reset to more basic config.

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ const path = require('path');
 import vue from "@vitejs/plugin-vue";
 
 export default ({ command }) => ({
-    publicDir: "disable",
-    base: command === "build" ? "/public/dist/" : "",
     plugins: [
         vue(),
     ],
     build: {
         manifest: true,
-        outDir: 'public/dist',
         rollupOptions: {
             input: 'src/main.js',
         },
